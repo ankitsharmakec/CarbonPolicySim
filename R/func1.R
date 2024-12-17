07b32763d6ea5bc51e531cbe1a8885f9bdde49c2
Ankit Sharma <sharmaankitjnv@gmail.com>
2024-12-17 20:47:39 +0530

    Initial commit

---
+++ R/func1.R
@@ -0,0 +1,16 @@
+#' Add Two Numbers
+#'
+#' This function adds two numbers together.
+#'
+#' @param a A numeric value.
+#' @param b A numeric value.
+#' @return The sum of a and b.
+#' @export
+#'
+#' @examples
+#' add_numbers(2, 3)
+
+
+add_numbers <- function(a, b) {
+  return(a + b)
+}
